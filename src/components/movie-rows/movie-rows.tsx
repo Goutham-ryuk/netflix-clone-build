8e7b80168b93ef7cc971a72c12d127ba453c2759
goutham-ryuk <gouthamwork018@gmail.com>
2024-06-24 00:08:18 +0530

    movie row fixes

--- src/components/movie-rows/movie-rows.tsx
+++ src/components/movie-rows/movie-rows.tsx
@@ -18,20 +18,24 @@ export const MovieRows: React.FC<IMovieRows> = ({
       <h2>{title}</h2>
       <div className="movie__row__posters">
         {movies &&
-          Object.keys(movies).map((key) => (
-            <img
-              className={`movie__row__poster ${
-                isLargeRow && "movie__row__posterLarge"
-              }`}
-              key={`${(movies as any)[key].id}`}
-              src={`${baseImageURL}${
-                isLargeRow
-                  ? (movies as any)[key].poster_path
-                  : (movies as any)[key].backdrop_path
-              }`}
-              alt={`${(movies as any)[key].name}`}
-            />
-          ))}
+          Object.keys(movies).map(
+            (key) =>
+              ((isLargeRow && (movies as any)[key].poster_path) ||
+                (!isLargeRow && (movies as any)[key].backdrop_path)) && (
+                <img
+                  className={`movie__row__poster ${
+                    isLargeRow && "movie__row__posterLarge"
+                  }`}
+                  key={`${(movies as any)[key].id}`}
+                  src={`${baseImageURL}${
+                    isLargeRow
+                      ? (movies as any)[key].poster_path
+                      : (movies as any)[key].backdrop_path
+                  }`}
+                  alt={`${(movies as any)[key].name}`}
+                />
+              )
+          )}
       </div>
     </div>
   );
